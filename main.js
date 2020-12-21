dae293a2fe8e841ba5313356bb6f77a681a10e2d
NicolaLampis <nico.lampo@gmail.com>
2020-12-21 21:54:04 +0000

    get data from api

---
+++ main.js
@@ -0,0 +1,13 @@
+var xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("data").innerHTML = this.responseText;
+    }
+};
+
+// open a connection
+
+xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+
+xhr.send();
