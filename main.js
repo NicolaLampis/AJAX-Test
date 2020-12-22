7e59b39026c16f5694112f4a63cf2f5d063539cf
NicolaLampis <nico.lampo@gmail.com>
2020-12-22 18:12:26 +0000

    get data from api using CALLBACKS

--- main.js
+++ main.js
@@ -1,13 +1,33 @@
-var xhr = new XMLHttpRequest();
+// var xhr = new XMLHttpRequest();
 
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("data").innerHTML = this.responseText;
-    }
-};
+// xhr.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//         document.getElementById("data").innerHTML = this.responseText;
+//     }
+// };
 
-// open a connection
+// // open a connection
 
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+// xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
 
-xhr.send();
+// xhr.send();
+
+
+function getData(cb) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+    xhr.send();
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
+}
+
+function printDataToConsole(data) {
+    console.log(data);
+}
+
+getData(printDataToConsole);
